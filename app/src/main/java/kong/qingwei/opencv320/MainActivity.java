26c912f0fe9422516f31d8fb336de4bfdf473cfb
kongqw <4878802@qq.com>
2017-05-19 13:44:36 +0800

    clear project

---
+++ app/src/main/java/kong/qingwei/opencv320/MainActivity.java
@@ -0,0 +1,16 @@
+package kong.qingwei.opencv320;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
